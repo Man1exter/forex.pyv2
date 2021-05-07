16f28341f2164e66ae726a2c1e9532bbed2dd0fa
Mariusz Perzynski <m.perzyk@interia.pl>
2021-05-07 18:41:49 +0200

    rename variables

--- into.py
+++ into.py
@@ -2,10 +2,10 @@ from forex_python.converter import CurrencyRates
 
 print("..Currency Converter From Forex..")
 
-forex_from = input("Write your suggestion from FOREX to convert => ")
-forex_to = input("Write your suggestion to MONEY withdraw => ")
+value_from = input("Write your suggestion from FOREX to convert => ")
+value_to = input("Write your suggestion to MONEY withdraw => ")
 
 cash = CurrencyRates()  
-case = cash.convert( forex_from , forex_to , 1 )
+case = cash.convert( value_from , value_to , 1 )
 
 print("Convert result...! =======>",case)
