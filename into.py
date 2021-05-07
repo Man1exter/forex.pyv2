a37235df3030a0f6388c0cca504422fafc34bfcd
Mariusz Perzynski <m.perzyk@interia.pl>
2021-05-07 09:20:13 +0200

    first push in new directiores

---
+++ into.py
@@ -0,0 +1,11 @@
+from forex_python.converter import CurrencyRates
+
+print("..Currency Converter From Forex..")
+
+forex_from = input("Write your suggestion from FOREX to convert => ")
+forex_to = input("Write your suggestion to MONEY withdraw => ")
+
+cash = CurrencyRates()  
+case = cash.convert( forex_from , forex_to , 1 )
+
+print("Convert {forex_from} to {forex_to} =======> ",case)
