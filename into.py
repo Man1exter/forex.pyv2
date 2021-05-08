d023912f83aa4e3f6316e3c51aa118c21f9cff65
Mariusz Perzynski <m.perzyk@interia.pl>
2021-05-08 10:13:15 +0200

    all elements connect with functions

--- into.py
+++ into.py
@@ -1,11 +1,16 @@
 from forex_python.converter import CurrencyRates
 
-print("..Currency Converter From Forex..")
+def function():
+  print("..Currency Converter From Forex..")
 
-value_from = input("Write your suggestion from FOREX to convert => ")
-value_to = input("Write your suggestion to MONEY withdraw => ")
+  value_from = input("Write your suggestion from FOREX to convert => ")
+  value_to = input("Write your suggestion to MONEY withdraw => ")
 
-cash = CurrencyRates()  
-case = cash.convert( value_from , value_to , 1 )
+  cash = CurrencyRates()  
+  case = cash.convert( value_from , value_to , 1 )
 
-print("Convert result...! =======>",case)
+  print("Convert result...! =======>",case)
+
+def main():
+    function()
+main()
