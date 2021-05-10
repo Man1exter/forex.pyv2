0e1b8ff4ae286de7e5c3144a8672b64d3bb0f1ed
Mariusz Perzynski <m.perzyk@interia.pl>
2021-05-10 10:11:27 +0200

    square with speed draw

--- turtle_way.py
+++ turtle_way.py
@@ -1,22 +1,22 @@
 import turtle
 
-# animal.forward(100)          # forward + px 
-# animal.left(90)              # left + deg
+# animal.forward(100)           # forward + px 
+# animal.left(90)               # left + deg
 # animal.forward(100)
 # animal.left(90)
 # animal.forward(100)
 # animal.left(90)
 
 def super_turtle(name,size,deg):
-    for i in range(50):
+    for i in range(300):
+        name.speed(5)
         name.forward(size)
         name.left(deg)
-
+        
 def main():
     animal = turtle.Turtle()     # turtle init to draw
     super_turtle(animal,100,90)
-    turtle.exitonclick()  # exit with [X]
-
+    turtle.exitonclick()         # exit with [X]
 main()
 
 # .pencolor( 'blue' )
