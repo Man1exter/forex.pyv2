390bb67632d619ba7805e6b281bb132eaa5a983e
Mariusz Perzynski <m.perzyk@interia.pl>
2021-05-10 10:04:27 +0200

    50x square in window

--- turtle_way.py
+++ turtle_way.py
@@ -1,7 +1,21 @@
 import turtle
 
-animal = turtle.Turtle()     # turtle init to draw
+# animal.forward(100)          # forward + px 
+# animal.left(90)              # left + deg
+# animal.forward(100)
+# animal.left(90)
+# animal.forward(100)
+# animal.left(90)
 
-animal.forward(100)
+def super_turtle(name,size,deg):
+    for i in range(50):
+        name.forward(size)
+        name.left(deg)
 
-turtle.exitonclick()  # exit with [X]
+def main():
+    animal = turtle.Turtle()     # turtle init to draw
+    super_turtle(animal,100,90)
+    turtle.exitonclick()  # exit with [X]
+
+main()
+
