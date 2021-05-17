d7f34decb268a106d2fd2c7c3da23c5b0905db5b
Mariusz Perzynski <m.perzyk@interia.pl>
2021-05-17 12:05:46 +0200

    copy elements with the same print

--- password.py
+++ password.py
@@ -10,4 +10,6 @@ numbers = "1234567890"
 all_elements = lower_case + upper_case + symbols + numbers
 
 password_print = " ".join(random.sample(all_elements,length))
+password_print_two = "".join(random.sample(all_elements,length))
 print(password_print)
+print(password_print_two)
