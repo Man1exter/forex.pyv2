9a2f32b51b8625a1f709cad45662c66f65e55765
Mariusz Perzynski <m.perzyk@interia.pl>
2021-05-17 11:59:55 +0200

    connect with join in one output password

--- password.py
+++ password.py
@@ -4,3 +4,9 @@ lower_case = "qwertyuiopasdfghjklzxcvbnm"
 upper_case = "ABCDEFGHIJKLMNTOPURWQZS"
 symbols = "{}[]\|/?><+=-)(*&^%$#@!"
 numbers = "1234567890"
+length = 20
+
+all_elements = lower_case + upper_case + symbols + numbers
+
+password_print = "".join(random.sample(all_elements,length))
+print(password_print)
