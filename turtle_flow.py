c0d257603499cd521897095716eceb38b61cfc0e
Mariusz Perzynski <m.perzyk@interia.pl>
2021-05-19 17:40:26 +0200

    collect all arguments in function

--- turtle_flow.py
+++ turtle_flow.py
@@ -1,16 +1,19 @@
 import turtle
 
-
-def first_move(length,position):
+def first_move(name,length,times,way):
     size = int(length / 2)
-
+    for element in range(times):
+        name.forward(size)
+        name.left(way)
 
 def main():
-   turtle_exp = turtle.Turtle()
+   turtles = turtle.Turtle()
 
    # place to show draw exercises: 
-    
+
+   first_move(turtles,100,2,10)
+
    ###############################
 
-   turtle_exp.exitonclick()
+   turtles.exitonclick()
 main()
