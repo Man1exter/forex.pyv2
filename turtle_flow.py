1777742fb874e4cd99322c3716d11a2e55f02484
Mariusz Perzynski <m.perzyk@interia.pl>
2021-05-19 17:26:55 +0200

    add place to show draw

--- turtle_flow.py
+++ turtle_flow.py
@@ -1,10 +1,16 @@
 import turtle
 
 
+def first_move(length,position):
+    size = int(length / 2)
 
 
 def main():
    turtle_exp = turtle.Turtle()
+
+   # place to show draw exercises: 
+    
+   ###############################
 
    turtle_exp.exitonclick()
 main()
