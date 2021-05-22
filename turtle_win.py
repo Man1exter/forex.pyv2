76f6aaa3b0244192be2b9ae41e53a3c126bf2559
Mariusz Perzynski <m.perzyk@interia.pl>
2021-05-22 09:58:45 +0200

    comment and add basic settings

--- turtle_win.py
+++ turtle_win.py
@@ -4,3 +4,13 @@ import random
 # init + settings
 animal = turtle.Turtle()
 animal.shape("turtle")
+
+# sett with turtle
+turtle.bgcolor("black")
+turtle.exitonclick()
+
+# about drawing set
+for it in range(50,500):
+  animal.color(random.choice(["blue","red","yellow","brown","purple","white"]))
+  animal.forward(it)
+  animal.right(120)
