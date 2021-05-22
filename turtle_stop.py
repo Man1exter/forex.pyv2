66b51bae3355fe23afeebd0420753f898990d3c0
Mariusz Perzynski <m.perzyk@interia.pl>
2021-05-22 09:46:05 +0200

    complete effect use turtle

--- turtle_stop.py
+++ turtle_stop.py
@@ -3,15 +3,22 @@ import random
 
 # init
 tur = turtle.Turtle()
+tur.shape('turtle')
 
 # settings
 turtle.bgcolor('black')
+tur.speed(10)
 
 # drawing loop
-for it in range(50,300):
+for it in range(50,500):
   tur.color(random.choice(["blue","red","yellow","brown","purple"]))
   tur.forward(it)
-  tur.right(91)
+  tur.right(92)
+
+# for it in range(50,500):
+# tur.color(random.choice(["blue","red","yellow","brown","purple"]))
+# tur.forward(it)
+# tur.right(120)
 
 # exit
 tur.exitonclick()
