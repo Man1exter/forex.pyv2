ced584e953dcb32b407370ff3104b88f72e202ee
Mariusz Perzynski <m.perzyk@interia.pl>
2021-05-22 10:46:15 +0200

    add to iteration + 5

--- turtle_win.py
+++ turtle_win.py
@@ -5,6 +5,7 @@ import random
 animal = turtle.Turtle()
 animal.shape("turtle")
 animal.speed(20)
+animal.pensize(5)
 
 # sett with turtle
 turtle.bgcolor("black")
@@ -12,7 +13,7 @@ turtle.bgcolor("black")
 # about drawing set
 for it in range(50,500):
   animal.color(random.choice(["blue","red","yellow","brown","purple","white"]))
-  animal.forward(it)
+  animal.forward(it + 5)
   animal.right(120)
 
 turtle.exitonclick()
