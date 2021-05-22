e327f623baad01f99053106d8eb24ceec304fcbe
Mariusz Perzynski <m.perzyk@interia.pl>
2021-05-22 10:44:42 +0200

    drawing done with random colors

--- turtle_win.py
+++ turtle_win.py
@@ -4,13 +4,15 @@ import random
 # init + settings
 animal = turtle.Turtle()
 animal.shape("turtle")
+animal.speed(20)
 
 # sett with turtle
 turtle.bgcolor("black")
-turtle.exitonclick()
 
 # about drawing set
 for it in range(50,500):
   animal.color(random.choice(["blue","red","yellow","brown","purple","white"]))
   animal.forward(it)
   animal.right(120)
+
+turtle.exitonclick()
