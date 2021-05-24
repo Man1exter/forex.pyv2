3ddddbbff760f87f8a2fff1b5681699af1944439
Mariusz Perzynski <m.perzyk@interia.pl>
2021-05-24 11:18:54 +0200

    add colors on bg/color

--- turtle_move.py
+++ turtle_move.py
@@ -5,32 +5,35 @@ import sys
 # initialization of game settings
 animal = turtle.Turtle()
 turtle.shape('turtle')
+turtle.bgcolor("black")
+turtle.color("white")
+#  turtle.screensize(2000,1500) 
 
 # game events
 
-keys = turtle.key.get_pressed()
+#keys = turtle.key.get_pressed()
 
  # --- WASD ---
 
-if keys[turtle.K_d]:
-   animal.x += 1
-if keys[turtle.K_s]:
-   animal.y += 1
-if keys[turtle.K_w]:
-   animal.y -= 1
-if keys[turtle.K_a]:
-   animal.x -= 1
+#if keys[turtle.K_d]:
+   #animal.x += 1
+#if keys[turtle.K_s]:
+   #animal.y += 1
+#if keys[turtle.K_w]:
+   #animal.y -= 1
+#if keys[turtle.K_a]:
+   #animal.x -= 1
 
  # --- ARROWS ---
 
-if keys[turtle.K_RIGHT]:
-       animal.x += 1
-if keys[turtle.K_DOWN]:
-   animal.y += 1
-if keys[turtle.K_UP]:
-   animal.y -= 1
-if keys[turtle.K_LEFT]:
-   animal.x -= 1
+#if keys[turtle.K_RIGHT]:
+       #animal.x += 1
+#if keys[turtle.K_DOWN]:
+   #animal.y += 1
+#if keys[turtle.K_UP]:
+   #animal.y -= 1
+#if keys[turtle.K_LEFT]:
+   #animal.x -= 1
 
 # exit with [x]
 turtle.exitonclick()
