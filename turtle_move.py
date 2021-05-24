32ff2322c5fd5e88ad437e6bfd26425f4aad32f1
Mariusz Perzynski <m.perzyk@interia.pl>
2021-05-24 11:32:51 +0200

    add effect with other section

--- turtle_move.py
+++ turtle_move.py
@@ -5,9 +5,12 @@ import sys
 # initialization of game settings
 animal = turtle.Turtle()
 turtle.shape('turtle')
-turtle.bgcolor("yellow")
-turtle.color("black")
+
+# cosmetics additionally
+turtle.bgcolor("lightblue")
+turtle.color("green")
 turtle.pensize('5')
+turtle.speed(5)
 #  turtle.screensize(2000,1500) 
 
 # game events
@@ -25,7 +28,7 @@ turtle.onkey(left,'Left')
 turtle.onkey(right,'Right')
 turtle.onkey(bye,'q')
 
-# to moving turtle way
+# to moving turtle way +add
 turtle.listen()
 turtle.mainloop()
 
